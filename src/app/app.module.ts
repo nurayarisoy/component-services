9c9d800ac4694c169b2fd19dcfa73ec5b2f7143a
Nuray arisoy <cevriyeceven@gmail.com>
2022-10-05 12:15:06 +0200

    api add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InfoBoxComponent } from './info-box/info-box.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookDataService } from './book-data.service';
+import { TodoService } from './todo.service';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfoBoxComponent,
+    BookListComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [BookDataService,TodoService,HttpClientModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/book-data.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: "root",
+})
+
+
+export class BookDataService {
+
+  books = [
+    {
+      title: "Book #1 from Service",
+      subtitle: "Subtitle #1",
+    },
+    {
+      title: "Book #2 from Service",
+      subtitle: "Subtitle #2",
+    },
+    {
+      title: "Book #3 from Service",
+      subtitle: "Subtitle #3",
+    },
+  ];
+
+  constructor(private http:HttpClient) {}
+
+  getBooks() {
+    return this.http.get("http://localhost:4730/books");
+  }
+}
